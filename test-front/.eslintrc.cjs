76dd56f906536b70abd9e464a59855ce7c2790a1
ringo9971 <ab7usa@gmail.com>
2024-05-15 21:43:02 +0900

    :bug: lint が通るように

---
+++ test-front/.eslintrc.cjs
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+  ],
+  settings: {
+    react: {
+      version: '18.x',
+    },
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    React: 'writable',
+  },
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'import'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          ['builtin', 'external'],
+          'internal',
+          ['parent', 'sibling', 'index', 'object', 'type'],
+        ],
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        pathGroups: [
+          { pattern: 'src/types/**', group: 'internal', position: 'before' },
+          {
+            pattern: 'src/repositories/**',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+      },
+    ],
+  },
+};
